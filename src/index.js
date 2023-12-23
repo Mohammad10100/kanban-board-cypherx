299a6df0c64d21b367fff5bf96a0a83d33654fe5
Mohammad <mohammadabdullateef@eng.rizvi.edu.in>
2023-12-23 21:45:43 +0530

    refactor- redux 'store' seperated from index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import rootReducer from './reducers/index'
-import { configureStore } from '@reduxjs/toolkit'
-
-const store = configureStore({
-  reducer: rootReducer
-})
+import store from './reducers/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
